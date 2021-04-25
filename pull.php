56d6d4a02cb21cebf11c6093706a5fc663853234
maxi1kiano <mnuniez@gmail.com>
2021-04-25 11:37:27 -0300

    commit pull.php

--- pull.php
+++ pull.php
@@ -1,7 +1,10 @@
 <?php
-echo 'Esto es una cadena sencilla';
 
-echo 'También se pueden incluir nuevas líneas en
-un string de esta forma, ya que es
-correcto hacerlo así';
+$array = [
+    "foo" => "bar",
+    "bar" => "foo",
+];
+
+var_dump($array);
+
 ?>
